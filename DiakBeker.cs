e4d254c1245f94f884559472d8c45041b62d35d2
Mása Barnabás Zsolt <masabarni0902>
2021-09-20 13:13:24 +0200

    első

---
+++ DiakBeker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_szerkezet
+{
+    class DiakBeker
+    {
+        public string Nev { get; set; }
+        public int EletKor { get; set; }
+        public DiakBeker(string nev, int eltkor)
+        {
+            this.Nev = nev;
+            this.EletKor = eltkor;
+
+        }
+
+        public void AdatKiir()
+        {
+            Console.WriteLine($"Nev:{this}")
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_szerkezet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiakBeker diak = new DiakBeker("Márkus Regő", 14);
+
+            Console.WriteLine(diak.Nev);
+            Console.WriteLine(diak.EletKor);
+
+            Console.ReadKey(true);
+        }
+    }
+}
